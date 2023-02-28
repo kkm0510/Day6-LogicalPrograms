2efcb643a3340182645a4abdb5c56dd0b946fba2
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-28 13:07:06 +0530

    ADDED : fibonacci() function

--- src/LogicalPrograms.java
+++ src/LogicalPrograms.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class LogicalPrograms {
+
+    static Scanner sc=new Scanner(System.in);
+
+    public void fibonacci() {
+        System.out.println("Enter number : ");
+        int n=sc.nextInt();
+        int i = 0;
+        int j = 1;
+        System.out.print(i + " " + j + " ");
+        for (int k = 3; k <= n; k++) {
+            j = j + i;
+            i = j - i;
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to logical programs");
+        LogicalPrograms logic = new LogicalPrograms();
+        logic.fibonacci();
     }
 }
