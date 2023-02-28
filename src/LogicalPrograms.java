d274ebbe06e84c65dab276defad441b0e2afd1cf
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-28 13:44:31 +0530

    ADDED : couponNumber() function

--- src/LogicalPrograms.java
+++ src/LogicalPrograms.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LogicalPrograms {
 
@@ -56,16 +58,61 @@ public class LogicalPrograms {
         System.out.println("Reverse of number = " + revNum);
     }
 
+
+    /*
+        Given :- coupon numbers, How many? -> N coupon numbers
+        Find? -> count of random numbers generated to get one distinct coupon number
+    */
+
+    //this func will generate numbers between 0 and (n+1)
+    //ex -> n=15, the input might have all elements from 0 to 14
+    //so the function will generate numbers from 0 to 15(14+1)
+    public static int generateRandomNumber(int n) {
+        return (int) ((Math.random() * 100) % (n + 1));
+    }
+
+    //return true if randomNum is not distinct, else return false if randomNum is distinct
+    public static boolean processDistinctNumbers(Set<Integer> set, int randomNum) {
+        System.out.println("Random number = " + randomNum);
+        if (set.contains(randomNum))
+            return true;
+        set.add(randomNum);
+        System.out.println("New coupon number : " + randomNum);
+        return false;
+    }
+
+    public static void takeInput(Set<Integer> set, int n) {
+        System.out.print("Enter numbers : ");
+        int i = 0;
+        while (i < n) {
+            set.add(sc.nextInt());
+            i++;
+        }
+    }
+
+    public void couponNumber() {
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        Set<Integer> set = new HashSet<>();
+        takeInput(set, n);
+        int count = 1;
+        while (processDistinctNumbers(set, generateRandomNumber(4))) {
+            count++;
+        }
+        System.out.println("Total random numbers needed to have all distinct numbers : " + count);
+    }
+
     public static void main(String[] args) {
         LogicalPrograms logic = new LogicalPrograms();
         System.out.println("Enter Choice : ");
-        System.out.print("1)Fibonacci Series  2)Perfect Number 3)Prime Number 4)Reverse a num : ");
+        System.out.print("1)Fibonacci Series  2)Perfect Number 3)Prime Number 4)Reverse a num 5)Coupon Numbers : ");
         int choice = sc.nextInt();
         switch (choice) {
             case 1 -> logic.fibonacci();
             case 2 -> logic.perfectNumber();
             case 3 -> logic.primeNumber();
             case 4 -> logic.reverseNumber();
+            case 5 -> logic.couponNumber();
         }
     }
 }
